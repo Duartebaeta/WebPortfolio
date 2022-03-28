6da701c2eac7cd08f8143f593d07cda97c0ba90b
Duarte Baeta <duartebaeta@Duartes-MBP.lan>
2022-03-28 03:11:18 +0100

    animação de hellos feita

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,28 @@
+import "./intro.css";
+
+const Intro = () => {
+	return [
+		<div className="i-container">
+			<div className="i-wrapper"> 
+				<div className="i-welcome">
+					<div className="i-welcome-wrapper">
+						<div className="i-welcome-item">Hello;</div>
+						<div className="i-welcome-item">Hola;</div>
+						<div className="i-welcome-item">Bonjour;</div>
+						<div className="i-welcome-item">Guten Tag;</div>
+						<div className="i-welcome-item">Olá;</div>
+						<div className="i-welcome-item">Salve;</div>
+						<div className="i-welcome-item">Nǐn Hǎo;</div>
+						<div className="i-welcome-item">Konnichiwa;</div>
+						<div className="i-welcome-item">G'day;</div>
+						<div className="i-welcome-item">Dobryj Den;</div>
+						<div className="i-welcome-item">Assalam u Alaikum;</div>
+					</div>
+				</div>
+				<h1 className="i-name">My Name Is Duarte Baeta</h1>
+			</div>
+		</div>,
+	];
+};
+
+export default Intro;
